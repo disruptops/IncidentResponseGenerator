3bbbd0b1753e3984e5748677517acdaabfdc1554
Rich Mogull <rmogull@iMac-Pro.localdomain>
2020-01-24 21:24:53 -0700

    Fixed a command line issue
    Also changed the path for the config file to use ./

--- incident_response_generator.py
+++ incident_response_generator.py
@@ -18,7 +18,7 @@ import yaml
 import time
 from botocore.exceptions import ClientError
 
-@click.command()
+
 
 def disable_cloudtrail(regions):
     try:
@@ -177,7 +177,7 @@ def create_lambda_attacks(config):
 
 if __name__ == "__main__":
    regions = ["us-east-2", "us-east-1", "us-west-1", "us-west-2", "ca-central-1", "ap-south-1", "ap-northeast-2", "ap-southeast-1", "ap-southeast-2", "ap-northeast-1", "eu-central-1", "eu-west-1", "eu-west-2", "sa-east-1"]
-   file = open('config.yml', 'r')
+   file = open('./config.yml', 'r')
    config = file.read()
    config = yaml.safe_load(config)
    disable_cloudtrail(regions)
